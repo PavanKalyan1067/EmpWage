1b57f6ddb0447c5e185ca6ef67c6d40be5f99350
PavanKalyan <pavankalyanyadav321@gmail.com>
2022-05-19 15:51:25 +0530

    Checking Employee is present or absent and calculating the wage for oneday and adding part-time and full-time employees and calculating wages for one month

--- EmpWage.py
+++ EmpWage.py
@@ -3,16 +3,27 @@ import random
 
 def employee_wage():
     attendance = []
-    wage_per_hour = 20
-    hours_per_day = 8
+    category = []
+    hours_worked = 0
+    days = 0
+    while True:
+        days += 1
+        if random.randrange(0, 2) == 0:
+            attendance.append('Absent')
+        else:
+            attendance.append('Present')
+            if random.randrange(0, 2) == 0:
+                category.append("FullTime")
+                hours_worked += 8
+                print(attendance[days - 1], "FullTime", days, hours_worked)
+            else:
+                category.append("PartTime")
+                hours_worked += 4
+                print(attendance[days - 1], "PartTime", days, hours_worked)
 
-    if random.randrange(0, 2) == 0:
-        attendance.append('Absent')
-        print("Employee is Absent")
-    else:
-        attendance.append('Present')
-        print("Employee is Present and wage per day is :")
-        print(wage_per_hour * hours_per_day)
+        if days == 20 or hours_worked == 100:
+            print("Month complete")
+            return attendance, category, days, hours_worked
 
 
 if __name__ == '__main__':
