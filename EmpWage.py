7c7bfd3a42b928121cf4ad66fbccc06e7845965a
PavanKalyan <pavankalyanyadav321@gmail.com>
2022-05-19 15:38:43 +0530

    Checking Employee is present or absent and calculating the wage for oneday

--- EmpWage.py
+++ EmpWage.py
@@ -2,15 +2,18 @@ import random
 
 
 def employee_wage():
-
     attendance = []
+    wage_per_hour = 20
+    hours_per_day = 8
 
     if random.randrange(0, 2) == 0:
         attendance.append('Absent')
         print("Employee is Absent")
     else:
         attendance.append('Present')
-        print("Employee is Present")
+        print("Employee is Present and wage per day is :")
+        print(wage_per_hour * hours_per_day)
 
 
-employee_wage()
+if __name__ == '__main__':
+    employee_wage()
